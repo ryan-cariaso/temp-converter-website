1e9448098beb5e4044d1c10246c5e53b57c7ecf4
ryan-cariaso <ryanchristophercariaso@gmail.com>
2021-07-07 14:44:02 -0700

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,12 +1,29 @@
 const btn = document.querySelector('button');
 const degree = document.getElementById('degree');
 const convert = document.getElementById('convert');
+const temperature = document.getElementById('temperature');
 let result = document.getElementById('result');
 
 degree.onclick = (event) => {
   degree.placeholder = ''
   degree.style.border = '2px solid gray';
 }
+
+function setError(err) {
+  err.style.border = '2px solid #ff0000';
+  err.placeholder = 'Error'
+}
+
+function selectOption(convert, newValue) {}
+
+function fahrenheitToCelsius(f) {
+  return (parseInt(f)-32) * 5/9;
+}
+
+function celsiusToFahrenheit(c){
+  return pareseInt(c) * 9/5 + 32;
+}
+
 
 btn.onclick = (event) => {
   event.preventDefault();
@@ -16,6 +33,11 @@ btn.onclick = (event) => {
   }
 
   else{
-    if(temperature.value === 'f'){}
+    if(temperature.value === 'f'){
+
+      
+
+
+    }
   }
 }
